2b2426409ee832048ec08ecbf43ba43cb443fb7e
sansej5000 <nrcminsk@gmail.com>
2022-05-30 23:52:18 +0300

    1

--- box_test.go
+++ box_test.go
@@ -5,7 +5,15 @@ import "testing"
 func TestNewBox(t *testing.T) {
 	res := NewBox(3)
 	if res.shapesCapacity != 3 {
-		t.Errorf("Error: ")
+		t.Errorf("Error: only a positive integer is accepted. Expected result 3")
 	}
+}
 
+func TestAddShape(t *testing.T) {
+	box := NewBox(3)
+	figure := Circle{Radius: 2}
+	ok := box.AddShape(figure)
+	if ok != nil {
+		t.Errorf("Error: only a positive integer is accepted")
+	}
 }
