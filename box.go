7cd20fd66816ba474eec158d1887a2890e7083b3
sansej5000 <99507931+sansej5000@users.noreply.github.com>
2022-05-30 16:46:16 +0300

    Update box.go

--- box.go
+++ box.go
@@ -17,39 +17,63 @@ func NewBox(shapesCapacity int) *box {
 // AddShape adds shape to the box
 // returns the error in case it goes out of the shapesCapacity range.
 func (b *box) AddShape(shape Shape) error {
-	panic("implement me")
+	if (len(b.shapes) < b.shapesCapacity){
+		b.shapes = append(b.shapes, shape)
+	}
+	return fmt.Errorf("quantity cannot be exceeded, current value: %d", len(b.shapes))
 }
 
 // GetByIndex allows getting shape by index
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) GetByIndex(i int) (Shape, error) {
-	panic("implement me")
+	if i < 0 || i >= len(b.shapes) {
+		return nil, fmt.Errorf("figure at index does not exist or index is out of range")
+	}
+	return b.shapes[i], nil
 
 }
 
 // ExtractByIndex allows getting shape by index and removes this shape from the list.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ExtractByIndex(i int) (Shape, error) {
-	panic("implement me")
+	if i < 0 || i >= len(b.shapes) {
+		return nil, fmt.Errorf("figure at index does not exist or index is out of range")
+	}
+	res := b.shapes[i]
+	b.shapes = append(b.shapes[:i], b.shapes[i+1:]...)
+	return res, nil
 
 }
 
 // ReplaceByIndex allows replacing shape by index and returns removed shape.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ReplaceByIndex(i int, shape Shape) (Shape, error) {
-	panic("implement me")
+	if i < 0 || i >= len(b.shapes) {
+		return nil, fmt.Errorf("figure at index does not exist or index is out of range")
+	}
+	res := b.shapes[i]
+	b.shapes[i] = shape
+	return res, nil
 
 }
 
 // SumPerimeter provides sum perimeter of all shapes in the list.
 func (b *box) SumPerimeter() float64 {
-	panic("implement me")
+	var res float64
+	for _, item := range b.shapes {
+		res = res + item.CalcPerimeter()
+	}
+	return res
 
 }
 
 // SumArea provides sum area of all shapes in the list.
 func (b *box) SumArea() float64 {
-	panic("implement me")
+	var res float64
+	for _, item := range b.shapes {
+		res = res + item.CalcArea()
+	}
+	return res
 
 }
 
